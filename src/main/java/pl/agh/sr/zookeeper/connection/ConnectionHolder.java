f9f84b1dfc525f6e9a4b08f0c7afdc0927ae879a
raduy <raduj.lukasz@gmail.com>
2015-06-12 10:02:57 +0200

    Add logger on closing connection.

--- src/main/java/pl/agh/sr/zookeeper/connection/ConnectionHolder.java
+++ src/main/java/pl/agh/sr/zookeeper/connection/ConnectionHolder.java
@@ -1,8 +1,11 @@
 package pl.agh.sr.zookeeper.connection;
 
 import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
 
 import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Creates and maintain connection with ZooKeeper server.
@@ -10,6 +13,8 @@ import java.io.IOException;
  * @author Lukasz Raduj 2015 raduj.lukasz@gmail.com.
  */
 public class ConnectionHolder implements AutoCloseable {
+    private static final Logger LOG = getLogger(ConnectionHolder.class);
+
     private static final int SESSION_TIMEOUT = 3000;
     private final ZooKeeper zooKeeper;
 
@@ -25,6 +30,7 @@ public class ConnectionHolder implements AutoCloseable {
 
     @Override
     public void close() throws Exception {
+        LOG.info("Closing connection to server");
         this.zooKeeper.close();
     }
 }
